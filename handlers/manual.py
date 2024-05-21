0ec219f13dd72ca0740212963e38500e5e00cc7b
Sergey Zakharov <kenthasla@vk.com>
2024-05-21 14:23:10 +0300

    edits

--- handlers/manual.py
+++ handlers/manual.py
@@ -1,18 +1,17 @@
 from aiogram import types, F
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import CommandStart
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from data.config import start_message, step_1, step_2, step_3, step_4, step_5, step_6, step_7, step_0, step_8
-from keyboards.inline_keyboards import start_keyboard, step_1_keyboard, step_2_keyboard, \
-    step_4_keyboard, step_5_keyboard, step_6_keyboard, step_7_keyboard, step_0_keyboard, \
-    step_3_roboforex_forex4you_keyboard, step_8_keyboard
+from data.config import start_message, step_2, step_3, step_0
+from keyboards.inline_keyboards import start_keyboard, step_1_keyboard, step_2_keyboard, step_0_keyboard
 from main_step_by_step_assistant import dp, bot
 from service.users_service import UserService
 
 
 async def get_step_3_keyboard(message: types.Message):
     buttons = [[
-        InlineKeyboardButton(text="RoboForex", callback_data="step_3_roboforex"),
+        InlineKeyboardButton(text="RoboForex", callback_data="step_3_roboforex_question"),
         InlineKeyboardButton(text="Forex4You", callback_data="step_3_forex4you"),
     ]]
 
@@ -31,7 +30,6 @@ async def get_step_3_keyboard(message: types.Message):
 
     buttons.append([
         InlineKeyboardButton(text="Назад", callback_data="step_3_back"),
-        InlineKeyboardButton(text="Продолжить", callback_data="step_3_continue"),
     ])
 
     return InlineKeyboardMarkup(inline_keyboard=buttons)
@@ -63,7 +61,7 @@ async def start(message: types.Message):
 @dp.callback_query(F.data == "start_no")
 async def start_no(call: types.CallbackQuery):
     await call.message.answer(
-        text="Вы можете ознакомится с нашими торговыми системами в этом боте \nhttps://t.me/RFclubbot")
+        text="Вы можете ознакомится с нашими торговыми системами в этом боте \nhttps://t.me/INFO_RF_bot")
 
 
 @dp.callback_query(F.data == "start_yes")
@@ -87,21 +85,25 @@ async def step_1_continue(call: types.CallbackQuery):
     telegram_id = str(call.message.chat.id)
     user = await UserService.find_one_or_none(**{"telegram_id": telegram_id})
     friend = await UserService.find_one_or_none(**{"telegram_id": user.friend})
-    link_royalfamily = friend.link_royalfamily if friend and friend.link_royalfamily else "https://royalfamily.club/login"
+    link_royalfamily = friend.link_royalfamily if friend and friend.link_royalfamily else "https://royalfamily.club/register?ref=d362c9ca-12b6-412e-aad2-31782a1c8acf"
 
     text = f"""
-<b>Регистрация кабинета RF</b>
+<b>ШАГ 1/6. Регистрация кабинета RF</b>
 
-Для регистрации на платформе "Royal Family" перейдите по этой ссылке
+В первую очередь надо зарегистрироваться на платформе «Royal Family». Для этого перейдите по ссылке:
 
 {link_royalfamily}
 
-В форме регистрации введите свою электронную почту, никнейм, телеграм и пароль. Примите условия соглашения и нажмите кнопку "Зарегистрироваться"
+В форме регистрации введите свою электронную почту, никнейм, телеграм и пароль. Примите условия соглашения и нажмите кнопку «Зарегистрироваться»
 """
-    await bot.send_photo(chat_id=call.message.chat.id,
-                         caption=text,
-                         photo="AgACAgIAAxkBAAICfGZHNfk4758O9rCktzf7l_UTkZ45AAIY3DEbTg85SuUS8KOJNIwAAQEAAwIAA3kAAzUE",
-                         reply_markup=step_1_keyboard)
+    try:
+        await bot.send_photo(chat_id=call.message.chat.id,
+                             caption=text,
+                             photo="AgACAgIAAxkBAAICfGZHNfk4758O9rCktzf7l_UTkZ45AAIY3DEbTg85SuUS8KOJNIwAAQEAAwIAA3kAAzUE",
+                             reply_markup=step_1_keyboard)
+    except TelegramBadRequest:
+        await call.message.answer(text=text,
+                                  reply_markup=step_1_keyboard)
 
 
 # Step 1
@@ -126,25 +128,57 @@ async def step_2_continue(call: types.CallbackQuery):
     await call.message.answer(text=step_3, reply_markup=keyboard)
 
 
-@dp.callback_query(F.data == "step_3_roboforex")
+@dp.callback_query(F.data == "step_3_roboforex_question")
+async def step_3_roboforex_question(call: types.CallbackQuery):
+    await call.message.delete()
+
+    text = """
+<b>Регистрация у брокера RoboForex</b>
+
+Регистрировались ли Вы ранее на брокере RoboForex?    
+"""
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Да", callback_data="step_3_roboforex_yes"),
+            InlineKeyboardButton(text="Нет", callback_data="step_3_roboforex_no")
+        ]
+    ])
+    await call.message.answer(text=text, reply_markup=keyboard)
+
+
+@dp.callback_query(F.data == "step_3_roboforex_yes")
+@dp.callback_query(F.data == "step_3_roboforex_no")
 async def step_3_roboforex(call: types.CallbackQuery):
     await call.message.delete()
+
+    question_result = call.data
 
     telegram_id = str(call.message.chat.id)
     user = await UserService.find_one_or_none(**{"telegram_id": telegram_id})
     friend = await UserService.find_one_or_none(**{"telegram_id": user.friend})
-    link_roboforex = friend.link_roboforex if friend and friend.link_roboforex else "https://my.roboforex.com/en/login/?a=ooem"
+    link_roboforex = friend.link_roboforex if friend and friend.link_roboforex else "https://my.roboforex.com/en/?a=vsve"
 
     text = f"""
+    
 <b>Регистрация у брокера RoboForex</b>
 
-Для регистрации перейдите по этой ссылке {link_roboforex}
+{"Для регистрации перейдите по этой ссылке:" if question_result == "step_3_roboforex_no" else "Для создания нового аккаунта перейдите по этой ссылке:"}
 
-и выполните все действия в соответствии с инструкцией https://teletype.in/@royalfamily.club/registraciya_robo
+{link_roboforex}
+
+и выполните все действия в соответствии с инструкцией
+{"https://teletype.in/@royalfamily.club/registraciya_robo" if question_result == "step_3_roboforex_no" else "https://teletype.in/@royalfamily.club/registraciya_robo2"}
 """
 
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Назад", callback_data="step_4_back"),
+            InlineKeyboardButton(text="Продолжить", callback_data=f"step_3_continue_roboforex"),
+        ]
+    ])
+
     await call.message.answer(text=text,
-                              reply_markup=step_3_roboforex_forex4you_keyboard)
+                              reply_markup=keyboard)
 
 
 @dp.callback_query(F.data == "step_3_forex4you")
@@ -164,42 +198,154 @@ async def step_3_forex4you(call: types.CallbackQuery):
 и выполните все действия в соответствии с инструкцией https://teletype.in/@royalfamily.club/oGUU_BSAGMD    
 """
 
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Назад", callback_data="step_4_back"),
+            InlineKeyboardButton(text="Продолжить", callback_data=f"step_3_continue_forex4you"),
+        ]
+    ])
+
     await call.message.answer(text=text,
-                              reply_markup=step_3_roboforex_forex4you_keyboard)
+                              reply_markup=keyboard)
 
 
-@dp.callback_query(F.data == "step_5_back")
-@dp.callback_query(F.data == "step_3_continue")
+@dp.callback_query(F.data == "step_5_back_roboforex")
+@dp.callback_query(F.data == "step_3_continue_roboforex")
+@dp.callback_query(F.data == "step_5_back_forex4you")
+@dp.callback_query(F.data == "step_3_continue_forex4you")
 async def step_3_continue(call: types.CallbackQuery):
     await call.message.delete()
-    await call.message.answer(text=step_4, reply_markup=step_4_keyboard)
+
+    data = call.data.split("_")[3]
+
+    text = f"""
+<b>ШАГ 4/6. Открытие личного торгового счета на {"RoboForex" if data == "roboforex" else "Forex4you"}</b>
+
+В этом шаге я помогу вам открыть счет, на котором будет происходить торговля вашим роботом. Хочу напомнить, что доступ к счету, пополнению и снятию средств имеете только вы.
+
+Ниже прикрепляю инструкцию:
+{"https://teletype.in/@royalfamily.club/sozdanie_scheta_robo" if data == "roboforex" else "https://teletype.in/@royalfamily.club/RgmjV5MDHBD"}
+"""
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Назад", callback_data="step_4_back"),
+            InlineKeyboardButton(text="Продолжить", callback_data=f"step_4_continue_{data}")
+        ]
+    ])
+
+    await call.message.answer(text=text, reply_markup=keyboard)
 
 
-@dp.callback_query(F.data == "step_6_back")
-@dp.callback_query(F.data == "step_4_continue")
+@dp.callback_query(F.data == "step_6_back_roboforex")
+@dp.callback_query(F.data == "step_4_continue_roboforex")
+@dp.callback_query(F.data == "step_6_back_forex4you")
+@dp.callback_query(F.data == "step_4_continue_forex4you")
 async def step_4_continue(call: types.CallbackQuery):
     await call.message.delete()
-    await call.message.answer(text=step_5, reply_markup=step_5_keyboard)
+
+    data = call.data.split("_")[3]
+
+    text = f"""
+<b>ШАГ 5/6. Пополнение торгового счета {"RoboForex" if data == "roboforex" else "Forex4you"}</b>
+
+Поздравляем! Вы уже открыли счет и стали на шаг ближе к запуску робота. Теперь вам нужно пополнить свой торговый счет на сумму, которая будет удобна для вас.
+
+Обратите внимание, что минимальная сумма для запуска робота в торговлю составляет 100$. Если у вас возникли вопросы или вам нужна помощь, не стесняйтесь обращаться к вашему пригласителю или в нашу техническую поддержку.
+
+Ниже прикрепляю инструкцию:
+{"https://teletype.in/@royalfamily.club/popolnenie_robo" if data == "roboforex" else "https://teletype.in/@royalfamily.club/jxagrJgv0Dc"}
+"""
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Назад", callback_data=f"step_5_back_{data}"),
+            InlineKeyboardButton(text="Продолжить", callback_data=f"step_5_continue_{data}"),
+        ]
+    ])
+
+    await call.message.answer(text=text, reply_markup=keyboard)
 
 
-@dp.callback_query(F.data == "step_7_back")
-@dp.callback_query(F.data == "step_5_continue")
+@dp.callback_query(F.data == "step_7_back_roboforex")
+@dp.callback_query(F.data == "step_5_continue_roboforex")
+@dp.callback_query(F.data == "step_7_back_forex4you")
+@dp.callback_query(F.data == "step_5_continue_forex4you")
 async def step_5_continue(call: types.CallbackQuery):
     await call.message.delete()
-    await call.message.answer(text=step_6, reply_markup=step_6_keyboard)
+
+    data = call.data.split("_")[3]
+
+    text = """
+<b>ШАГ 6/6. Запуск робота</b>
+
+Остался последний шаг - запустить робота, чтобы он начал торговать. До первой прибыльной сделки осталось совсем немного.
+
+Инструкцию прикрепляю ниже:
+https://teletype.in/@royalfamily.club/3jcDxKHRHQp
+"""
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Назад", callback_data=f"step_6_back_{data}"),
+            InlineKeyboardButton(text="ВАЖНО", callback_data=f"step_6_continue_{data}"),
+        ]
+    ])
+
+    await call.message.answer(text=text, reply_markup=keyboard)
 
 
-@dp.callback_query(F.data == "step_8_back")
-@dp.callback_query(F.data == "step_6_continue")
+@dp.callback_query(F.data == "step_8_back_roboforex")
+@dp.callback_query(F.data == "step_6_continue_roboforex")
+@dp.callback_query(F.data == "step_8_back_forex4you")
+@dp.callback_query(F.data == "step_6_continue_forex4you")
 async def step_6_continue(call: types.CallbackQuery):
     await call.message.delete()
-    await call.message.answer(text=step_7, reply_markup=step_7_keyboard)
+
+    data = call.data.split("_")[3]
+
+    text = """
+<b>Важно! На что обращать внимание при роботе с роботом</b>
+
+https://teletype.in/@royalfamily.club/jJbrA8mUqfh    
+"""
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Назад", callback_data=f"step_7_back_{data}"),
+            InlineKeyboardButton(text="Продолжить", callback_data=f"step_7_continue_{data}"),
+        ]
+    ])
+
+    await call.message.answer(text=text, reply_markup=keyboard)
 
 
-@dp.callback_query(F.data == "step_7_continue")
+@dp.callback_query(F.data == "step_7_continue_roboforex")
+@dp.callback_query(F.data == "step_7_continue_forex4you")
 async def step_7_continue(call: types.CallbackQuery):
     await call.message.delete()
-    await call.message.answer(text=step_8, reply_markup=step_8_keyboard)
+
+    data = call.data.split("_")[3]
+
+    text = """
+<b>Вы завершили все шаги необходимые для запуска робота!</b>
+Желаем успешного использования и высокой прибыли!
+
+Если вы еще не добавлены в закрытый чат напишите в техническую поддержку для добавления.
+
+Для управления партнерским разделом в боте отправьте сообщение:  /partner
+"""
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Написать в техподдержку", url="https://t.me/RoyalFamily_Support_bot")
+        ],
+        [
+            InlineKeyboardButton(text="Назад", callback_data=f"step_8_back_{data}"),
+        ],
+    ])
+
+    await call.message.answer(text=text, reply_markup=keyboard)
 
 
 @dp.message()
--- keyboards/inline_keyboards.py
+++ keyboards/inline_keyboards.py
@@ -28,51 +28,6 @@ step_2_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     ]
 ])
 
-step_3_roboforex_forex4you_keyboard = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [
-            InlineKeyboardButton(text="Назад", callback_data="step_4_back"),
-            InlineKeyboardButton(text="Продолжить", callback_data="step_3_continue"),
-        ]
-    ])
-
-step_4_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [
-        InlineKeyboardButton(text="Назад", callback_data="step_4_back"),
-        InlineKeyboardButton(text="Продолжить", callback_data="step_4_continue")
-    ]
-])
-
-step_5_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [
-        InlineKeyboardButton(text="Назад", callback_data="step_5_back"),
-        InlineKeyboardButton(text="Продолжить", callback_data="step_5_continue"),
-    ]
-])
-
-step_6_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [
-        InlineKeyboardButton(text="Назад", callback_data="step_6_back"),
-        InlineKeyboardButton(text="Продолжить", callback_data="step_6_continue"),
-    ]
-])
-
-step_7_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [
-        InlineKeyboardButton(text="Назад", callback_data="step_7_back"),
-        InlineKeyboardButton(text="Продолжить", callback_data="step_7_continue"),
-    ]
-])
-
-step_8_keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [
-        InlineKeyboardButton(text="Написать в техподдержку", url="https://t.me/RoyalFamily_Support_bot")
-    ],
-    [
-        InlineKeyboardButton(text="Назад", callback_data="step_8_back"),
-    ],
-])
-
 change_links_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton(text="Изменить ссылку на RoyalFamily", callback_data="change_royalfamily"),
